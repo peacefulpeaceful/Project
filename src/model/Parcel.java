b213a0abac0a624f6e2f4a7505a5c7609b955522
turlantlepbergen <turlan.t08@icloud.com>
2026-02-09 23:05:40 +0500

    Parcel

--- src/model/Parcel.java
+++ src/model/Parcel.java
@@ -12,9 +12,6 @@ public class Parcel {
     private LocalDateTime createdAt;
     private double cost;
 
-    public Parcel() {
-    }
-
     private Parcel(Builder builder) {
         this.weight = builder.weight;
         this.status = builder.status;
@@ -41,44 +38,24 @@ public class Parcel {
         return category;
     }
 
+    public Client getSender() {
+        return sender;
+    }
 
+    public Client getRecipient() {
+        return recipient;
+    }
 
 
     public void setId(int id) {
         this.id = id;
     }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public void setStatus(ParcelStatus status) {
-        this.status = status;
-    }
-
-    public void setCategory(ParcelCategory category) {
-        this.category = category;
-    }
-
-    public void setSender(Client sender) {
-        this.sender = sender;
-    }
-
-    public void setRecipient(Client recipient) {
-        this.recipient = recipient;
-    }
-
-    public double getCost() {
+    public double getCost(){
         return cost;
     }
 
-    public void setCost(double cost) {
-        this.cost = cost;
-    }
-
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
     }
 
     public static class Builder {
